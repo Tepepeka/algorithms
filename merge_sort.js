303aef6158920a4e5ed329a66960d82b870af33b
Tepepeka <jeremy.bourgoin@live.fr>
2021-07-16 02:32:03 +0200

    first commit

---
+++ merge_sort.js
@@ -0,0 +1,46 @@
+// leftArr and rightArr are sorted!
+const merge = (leftArr, rightArr) => {
+   const output = [];
+   let leftIndex = 0;
+   let rightIndex = 0;
+   
+   while(leftIndex < leftArr.length && rightIndex < rightArr.length) {
+       const leftEl = leftArr[leftIndex];
+       const rightEl = rightArr[rightIndex];
+
+       if (leftEl < rightEl) {
+           output.push(leftEl);
+           leftIndex++;
+       } else {
+           output.push(rightEl);
+           rightIndex++;
+       }
+   }
+   return [...output, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)];
+};
+
+// recursive
+const mergeSort = array => {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const middleIndex = Math.floor(array.length / 2);
+    const leftArr = array.slice(0, middleIndex);
+    const rightArr = array.slice(middleIndex);
+
+    return merge(
+        mergeSort(leftArr),
+        mergeSort(rightArr)
+    );
+};
+
+console.log(mergeSort([1, 45, 98, 65, 45, 9, 7, 31]));
+
+//[1, 4, 2, 8, 345]
+//[1, 4, 2] [8, 345]
+//[1, 4] [2] [8] [345]
+//[1] [4] [2] [8] [345]
+//[1, 4] [2, 8] [345]
+//[1, 2, 4, 8] [345]
+//[1, 2, 4, 8, 345]
